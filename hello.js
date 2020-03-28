ed206efb4feba5f7df3bb0903d769c980f81716e
diegonieto98 <diegisniet@hotmail.com>
2020-03-27 23:47:13 -0500

    mi segundo commit

--- hello.js
+++ hello.js
@@ -1,1 +1,11 @@
 console.log('Hello world');
+var readline = require ('readline'), rl = readline.createInterface(process.stdin,process.stdout);
+rl.setPrompt('Inicio el programa: (on/off)');
+rl.prompt()
+rl.on('line', function(start){
+    switch(start.trim()){
+        case 'on':
+            console.log('su programa esta: '+String(start));
+            break;
+    };
+});
