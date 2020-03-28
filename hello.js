d696532ae00fa45d1b2c44c24390c6bb5fa315b5
diegonieto98 <diegisniet@hotmail.com>
2020-03-28 00:11:18 -0500

    lo ultimo que agregue toca verificarlo

--- hello.js
+++ hello.js
@@ -3,9 +3,21 @@ var readline = require ('readline'), rl = readline.createInterface(process.stdin
 rl.setPrompt('Inicio el programa: (on/off)');
 rl.prompt()
 rl.on('line', function(start){
-    switch(start.trim()){
-        case 'on':
-            console.log('su programa esta: '+String(start));
-            break;
-    };
+    var start = start.trim();
+    console.log('su computador esta: '+start);
 });
+rl.setPrompt('Quiere saber su edad (si/no): ');
+rl.prompt()
+rl.on(function(resp){
+    switch(resp.trim()){
+        case 'si':
+            rl.setPrompt('ingrese el año de nacimiento: '), rl.prompt()
+            rl.on('line', function(año){
+                var año = int(año.trim())
+                var edad = 2020 - año;
+                console.log('la edad que tiene es : '+edad)
+            })
+
+        case 'no':
+    }
+})
